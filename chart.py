d612adc1db28be8b61be214b8077aaea183e52c4
Nate Solon <nate@Nates-MacBook-Pro.local>
2023-03-18 15:20:02 -0500

    new charts and fixes

--- chart.py
+++ chart.py
@@ -1,17 +1,19 @@
 import matplotlib.pyplot as plt
 
 
-def plot_length(df):
+def plot_length(df, of):
     plt.style.use("ggplot")
     plt.rcParams["figure.figsize"] = (20, 5)
-    ax = df.length.hist(bins=20, color="red")
+    fig, ax = plt.subplots()
+    
+    df.length.hist(bins=20, color="red", ax=ax)
     ax.set_title("Length")
     ax.set_xlabel("Moves")
     ax.set_ylabel("Games")
-    fig = ax.get_figure()
-    fig.savefig("length.png")
+    ax.set_xlim([0,500])
+    fig.savefig(of)
 
-def plot_results(df):
+def plot_results(df, of):
     results = df.result.value_counts()
     total = results.sum()
 
@@ -25,11 +27,23 @@ def plot_results(df):
     fig, ax = plt.subplots()
     ax.set_title("Results")
 
-    W = plt.barh(0, [w_pct], color="whitesmoke", label="test")
+    W = plt.barh(0, [w_pct], color="whitesmoke")
     ax.bar_label(W, label_type="center", color="black", fmt='%.0f%%')
     D = plt.barh(0, [d_pct], left=[w_pct], color="gray")
+    if d_pct > 10:
+        ax.bar_label(D, label_type="center", color="white", fmt='%.0f%%')
     B = plt.barh(0, [b_pct], left=[w_pct + d_pct], color="black")
     ax.bar_label(B, label_type="center", color="white", fmt='%.0f%%')
 
     plt.axis("off")
-    fig.savefig("results.png")
+    fig.savefig(of)
+
+def plot_first_moves(df, of):
+    plt.style.use("ggplot")
+    plt.rcParams["figure.figsize"] = (20, 5)
+    fig, ax = plt.subplots()
+    df.first_move.value_counts().sort_values().plot.barh(ax=ax)
+    ax.set_title("First Moves")
+    ax.set_xlabel("Games")
+    ax.set_ylabel("Move")
+    fig.savefig(of)
--- process.py
+++ process.py
@@ -2,6 +2,8 @@ import chess.pgn
 from pathlib import Path
 import pandas as pd
 
+from chart import plot_first_moves, plot_length, plot_results
+
 
 results_dict = {
     "1-0": 1,
@@ -9,31 +11,43 @@ results_dict = {
     "1/2-1/2": 0.5,
 }
 
-def process_game(fn):
-    pgn = open(fn)
+def process_game(f):
+    pgn = open(f)
     game = chess.pgn.read_game(pgn)
-    length = len(list(game.mainline_moves()))
+    moves = list(game.mainline_moves())
+    length = len(moves)
+    first_move = chess.Board().san(moves[0])
     if game.headers["Result"] != "*":
         result = results_dict[game.headers["Result"]]
     else:
         result = 1 if length % 2 == 1 else 0
-    return length, result
+    return length, result, first_move
 
-def process_dir(path, outfile):
+def get_data(path, outfile):
     path = Path(path)
-    lengths, results = [], []
+    lengths, results, first_moves = [], [], []
     i = 0
     for f in path.glob("*.pgn"):
         i += 1
         if i % 1000 == 0:
             print(i)
-        if i == 10_000:
-            break
-        length, result = process_game(f)
+        length, result, first_move = process_game(f)
         lengths.append(length)
         results.append(result)
+        first_moves.append(first_move)
     df = pd.DataFrame({
         "length": lengths,
         "result": results,
+        "first_move": first_moves,
     })
     df.to_csv(outfile, index=False)
+
+def process_dir(pgn_path, op_dir):
+    name = Path(pgn_path).name
+    p = op_dir/name
+    p.mkdir(exist_ok=True)
+    get_data(pgn_path, p/"data.csv")
+    df = pd.read_csv(p/"data.csv")
+    plot_length(df, p/"length.png")
+    plot_results(df, p/"results.png")
+    plot_first_moves(df, p/"first_moves.png")
